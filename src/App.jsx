6652a348f3f294c388304e65711d46ff166e2914
Elephant24456 <rlaxortn0984@naver.com>
2024-05-20 15:52:20 +0900

    제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-export default function App() {
-  return (
-    <div>
-      <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
-      <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
-      </div>
-      <hr />
-      <div>
-        <h3>결과</h3>
-        <p>0</p>
-      </div>
-    </div>
-  );
-}
+// import { useState } from "react";
+
+// export default function App() {
+//   const [number, setNumber] = UseState(0);
+//   const [inputValue, setInputValue] = useState('');
+  
+//   const Addition = () => {
+//     setNumber(number + parseInt);
+//   }
+  
+//   return (
+//     <div>
+//       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
+//       <div>
+//         <input type="number"/> 만큼을 <button onClick={Addition}>더할게요</button> <button onClick={Substaction}>뺄게요</button>
+//         <button onClick={Reset}>초기화</button>
+//       </div>
+//       <hr />
+//       <div>
+//         <h3>결과</h3>
+//         <p>0</p>
+//       </div>
+//     </div>
+//   );
+// }
