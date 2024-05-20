d2ce5fe3bf4f1adb1ed7376810e95399b3b66185
Elephant24456 <rlaxortn0984@naver.com>
2024-05-20 16:53:08 +0900

    투두제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-export default function App() {
-  return (
-    <>
-      <h1>투두리스트 타임어택</h1>
-    </>
-  );
-}
+
+// export default function App() {
+//   const initialState = [
+//     {id: 0, title: "", body: "", isDone: false}
+//   ];
+//   const [users, setUsers] = useState(initialState);
+
+//   const [title, setTitle] = useState("");
+//   const [content, seContent] = useState("");
+
+//   const removeUser = (id) => {
+//     setUsers(users.filter(user = > user.id !== id));
+//   };
+
+//   return (
+//     <>
+//       <h1>투두리스트 타임어택</h1>
+//     </>
+//   );
+// }
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,3 +1,12 @@
-export default function TodoForm() {
-  return <div>TodoForm</div>;
-}
+// export default function TodoForm() {
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+
+//     const formData = new FormData(e.target);
+//     const title = formData.get("title");
+//     const content = formData.get("content");
+
+//     if (!title.trim() || !content.trim()) return alert("제목과 내용을 입력해주세요.")
+//   }
+//   return <div>TodoForm</div>;
+// }
